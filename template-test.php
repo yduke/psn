3ecdcd496b9db9bce6f3c56a052b4cb628bacecf
Duke Yin <yduke@hotmail.com>
2023-04-05 00:53:49 +0800

    Update template-test.php

--- template-test.php
+++ template-test.php
@@ -3,3 +3,19 @@
 Template Name: TEST
 */
 header( 'Content-Type: application/json' );
+    $args = array(
+        'post_type' => 'steam_game',
+        'meta_query' => array(
+            array(
+                'key' => 'img_library',
+                'value' => '',
+                'compare' => 'NOT EXISTS'
+            )
+        ),
+        'posts_per_page' => -1,
+        'orderby'=>'rand'
+    );
+    $posts = get_posts($args);
+	$post_id = reset($posts)->ID;
+	$appid = get_post_meta($post_id, 'appid',true);
+	var_dump($appid);
