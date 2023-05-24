3d63ab42a4304eace0c7e8ad9e8c3ca91e55c6a0
Duke Yin <yduke@hotmail.com>
2023-05-24 10:25:23 +0800

    Update template-test.php

--- template-test.php
+++ template-test.php
@@ -4,6 +4,7 @@ Template Name: TEST
 */
 // header( 'Content-Type: application/json' );
 // $appid  = 19000;
-$r = GetSpecialSales();
-
+$r = GetRecentlyPlayedGames();
+echo '<pre>';
 var_dump($r);
+echo '</pre>';
